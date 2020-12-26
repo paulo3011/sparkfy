bf9041d9f973d81fdb699193d3b13262df7c7ac1
Paulo Moreira <paulo.moreira@ceabs.com.br>
2020-12-26 10:56:36 -0300

    created sql for create tables

---
+++ project1/docker/initdb/01.sql
@@ -0,0 +1,87 @@
+
+-- https://classroom.udacity.com/nanodegrees/nd027/parts/f7dbb125-87a2-4369-bb64-dc5c21bb668a/modules/a7801de4-ee3f-4531-b887-82dea67f47a6/lessons/01995bb4-db30-4e01-bf38-ff11b631be0f/concepts/1533c19b-0505-49fd-b1b7-06c987641f0d
+
+-- https://www.postgresql.org/docs/13/ddl-basics.html
+-- https://www.postgresql.org/docs/13/ddl-constraints.html (PK,FK)
+-- https://www.postgresql.org/docs/13/datatype.html
+
+-- create schema songplay;
+
+-- drop to reset the database if exists
+drop database if exists sparkifydb;
+
+-- https://www.postgresql.org/docs/13/manage-ag-templatedbs.html
+-- CREATE DATABASE actually works by copying an existing database. By default, it copies the standard system database named template1.
+-- By instructing CREATE DATABASE to copy template0 instead of template1, you can create a “pristine” user database (one where no user-defined objects exist and where the system objects have not been altered) that contains none of the site-local additions in template1.
+create database sparkifydb with encoding 'utf8' TEMPLATE template0;
+
+-- dimension tables
+
+-- users - users in the app
+-- https://www.postgresql.org/docs/13/datatype-character.html
+-- in fact character(n) is usually the slowest of the three because of its additional storage costs. In most situations text or character varying should be used instead.
+create table users
+(
+    user_id integer -- can have up to 2.147.483.647 users
+    ,first_name varchar(60)
+    ,last_name varchar(60)
+    ,gender varchar(1)
+    ,level varchar(4) -- paid or free
+);
+
+-- songs - songs in music database
+-- https://www.postgresql.org/docs/13/datatype-numeric.html
+-- The type numeric can store numbers with a very large number of digits. It is especially recommended for storing monetary amounts and other quantities where exactness is required. Calculations with numeric values yield exact results where possible, e.g., addition, subtraction, multiplication. However, calculations on numeric values are very slow compared to the integer types, or to the floating-point types
+-- Floating-point types (real, double) ar inexact. Inexact means that some values cannot be converted exactly to the internal format and are stored as approximations, so that storing and retrieving a value might show slight discrepancies
+create table songs
+(
+    song_id varchar(30)
+    ,title varchar(60)
+    ,artist_id varchar(30)
+    ,year smallint
+    ,duration numeric(3,6) -- ex: 218.93179
+);
+
+-- artists - artists in music database
+-- https://docs.mapbox.com/help/glossary/lat-lon/
+-- https://stackoverflow.com/questions/15965166/what-is-the-maximum-length-of-latitude-and-longitude
+-- https://en.wikipedia.org/wiki/Geographic_coordinate_system
+create table artists
+(
+    artist_id varchar(30)
+    ,name varchar(60)
+    ,location varchar(60)
+    ,latitude numeric(3,6)  -- ex: 35.14968
+    ,longitude numeric(3,6) -- ex: -90.04892
+);
+
+-- time - timestamps of records in songplays broken down into specific units
+create table time
+(
+    start_time timestamp not null -- represent the timestamp where one song was played
+    ,hour smallint not null
+    ,day smallint not null
+    ,week smallint not null
+    ,month smallint not null
+    ,year smallint not null
+    ,weekday smallint not null
+);
+
+-- fact tables
+
+create fact_songplays
+(
+    songplay_id bigserial NOT NULL --bigserial == default nextval('fact_songplays_id')
+    ,start_time timestamp, 
+    ,user_id integer        REFERENCES users (user_id)
+    ,level varchar(4)       -- paid or free
+    ,song_id varchar(30)    REFERENCES songs (song_id)
+    ,artist_id varchar(30)  REFERENCES artists (artist_id)
+    ,session_id integer
+    ,location varchar(60)
+    ,user_agent varchar(400)
+    PRIMARY KEY (songplay_id)
+);
+
+-- COMMENT ON TABLE 
+COMMENT ON COLUMN fact_songplays.start_time IS 'When the song was played';
--- project1/readme.md
+++ project1/readme.md
@@ -21,3 +21,25 @@ In this project, you'll apply what you've learned on data modeling with Postgres
 - [] Create an ETL pipeline that transfers data from files in __two local directories__ into these tables in Postgres using Python and SQL
 - [] You'll be able to test your database and ETL pipeline by running queries given to you by the analytics team from Sparkify
 - [] Compare your results with their expected results
+
+
+# Requirements
+
+## Fact Table
+
+songplays - records in log data associated with song plays i.e. records with page NextSong
+- songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
+
+## Dimension Tables
+
+users - users in the app
+- user_id, first_name, last_name, gender, level
+
+songs - songs in music database
+- song_id, title, artist_id, year, duration
+
+artists - artists in music database
+- artist_id, name, location, latitude, longitude
+
+time - timestamps of records in songplays broken down into specific units
+- start_time, hour, day, week, month, year, weekday
