69f6e751ba0b045561fb0920eff251e1a391df41
Paulo Moreira <paulo.moreira@ceabs.com.br>
2021-04-19 09:44:03 -0300

    project06: StageToRedshiftOperator ok

--- project6-data-pipelines/runtime/dags/udac_example_dag.py
+++ project6-data-pipelines/runtime/dags/udac_example_dag.py
@@ -8,39 +8,45 @@ from operators.load_dimension import LoadDimensionOperator
 from operators.data_quality import DataQualityOperator
 from helpers.sql_queries import SqlQueries
 
-# AWS_KEY = os.environ.get('AWS_KEY')
-# AWS_SECRET = os.environ.get('AWS_SECRET')
+# AWS_KEY = os.environ.get("AWS_KEY")
+# AWS_SECRET = os.environ.get("AWS_SECRET")
+
+# [ok] https://classroom.udacity.com/nanodegrees/nd027/parts/45d1c3b1-d87b-4578-a6d0-7e86bb5fea6c/modules/57c3b9d1-4d8b-4afe-bfb4-92cfac622c7f/lessons/4d1d5892-2cab-4456-8b1a-fb2b5fa1488d/concepts/3b78f18c-3a53-40ab-8300-b1fe5208de97
 
 default_args = {
-    'owner': 'udacity',
-    'start_date': datetime(2018, 11, 1),
-    'end_date': datetime(2018, 11, 2)
+    "owner": "udacity",
+    "start_date": datetime(2018, 11, 1),
+    "end_date": datetime(2018, 11, 2),
+    "depends_on_past": False,
+    "retries": 3,
+    "retry_delay": timedelta(minutes=5),
+    "email_on_retry": False
 }
 
-dag = DAG('udac_example_dag',
+dag = DAG("udac_example_dag",
           default_args=default_args,
-          description='Load and transform data in Redshift with Airflow.',
-          schedule_interval='0 23 * * *'
+          description="Load and transform data in Redshift with Airflow.",
+          schedule_interval="0 23 * * *",
           # schedule_interval=None
-          # ,catchup=False
+          catchup=False
         )
 
-start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+start_operator = DummyOperator(task_id="Begin_execution",  dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_events',
+    task_id="Stage_events",
     dag=dag,
     source_path="s3://moreira-ud/udacity-dend/log_data/",
     target_table="stage_events",
     conn_id="redshift",
-    # partition_by="{{execution_date.strftime('%Y/%m')}}/{{execution_date.strftime('%Y')}}-{{execution_date.strftime('%m')}}-{{execution_date.strftime('%d')}}-events.json",
-    partition_by="{{execution_date.strftime('%Y/%m')}}/",
+    # partition_by="{{execution_date.strftime("%Y/%m")}}/{{execution_date.strftime("%Y")}}-{{execution_date.strftime("%m")}}-{{execution_date.strftime("%d")}}-events.json",
+    partition_by='{{execution_date.strftime("%Y/%m")}}/',
     jsonpaths="s3://moreira-ud/udacity-dend/log_json_path.json",
     retries=0
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_songs',
+    task_id="Stage_songs",
     dag=dag,
     source_path="s3://moreira-ud/udacity-dend/song_data/",
     target_table="stage_songs",
@@ -51,36 +57,36 @@ stage_songs_to_redshift = StageToRedshiftOperator(
 )
 
 load_songplays_table = LoadFactOperator(
-    task_id='Load_songplays_fact_table',
+    task_id="Load_songplays_fact_table",
     dag=dag
 )
 
 load_user_dimension_table = LoadDimensionOperator(
-    task_id='Load_user_dim_table',
+    task_id="Load_user_dim_table",
     dag=dag
 )
 
 load_song_dimension_table = LoadDimensionOperator(
-    task_id='Load_song_dim_table',
+    task_id="Load_song_dim_table",
     dag=dag
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
-    task_id='Load_artist_dim_table',
+    task_id="Load_artist_dim_table",
     dag=dag
 )
 
 load_time_dimension_table = LoadDimensionOperator(
-    task_id='Load_time_dim_table',
+    task_id="Load_time_dim_table",
     dag=dag
 )
 
 run_quality_checks = DataQualityOperator(
-    task_id='Run_data_quality_checks',
+    task_id="Run_data_quality_checks",
     dag=dag
 )
 
-end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+end_operator = DummyOperator(task_id="Stop_execution",  dag=dag)
 
 
 start_operator >> [stage_events_to_redshift, stage_songs_to_redshift]
